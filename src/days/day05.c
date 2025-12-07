3beac44da3539ea9144df7af3422b69ac199af29
Heath Raftery <heath@empirical.ee>
2025-12-07 14:31:35 +1100

    Day 5, part 2

--- src/days/day05.c
+++ src/days/day05.c
@@ -17,7 +17,6 @@ void part1(FILE *f)
   char line[MAX_LINE_LENGTH];
 
   int numRanges = 0;
-  
   while (fgets(line, MAX_LINE_LENGTH, f) != NULL)
   {
     if(sscanf(line, "%llu-%llu\n", &lo[numRanges], &hi[numRanges]) != 2)
@@ -53,5 +52,86 @@ void part1(FILE *f)
 
 void part2(FILE *f)
 {
-  printf("This is day %d, part 2.\n", DAY);
+  uint64_t lo[MAX_RANGES];
+  uint64_t hi[MAX_RANGES];
+  size_t llLoAsc[MAX_RANGES];
+  size_t llLoAscHead = 0;
+  
+  char line[MAX_LINE_LENGTH];
+
+  int numRanges = 0;
+  while (fgets(line, MAX_LINE_LENGTH, f) != NULL)
+  {
+    if(sscanf(line, "%llu-%llu\n", &lo[numRanges], &hi[numRanges]) != 2)
+      break;
+
+    //Create linked list of indices in order of lo value as we go.
+    if(numRanges > 0)
+    {
+      size_t llIdxPrev;
+      for(int i=0; i<numRanges; i++)
+      {
+        uint64_t thisLo = i==0 ? lo[llLoAscHead] : lo[llLoAsc[llIdxPrev]];
+
+        if(lo[numRanges] <= thisLo) //then insert here
+        {
+          if(i==0) //then update head
+          {
+            llLoAsc[numRanges] = llLoAscHead;
+            llLoAscHead = numRanges;
+          }
+          else //update ll
+          {
+            llLoAsc[numRanges] = llLoAsc[llIdxPrev];
+            llLoAsc[llIdxPrev] = numRanges;
+          }
+          break;
+        }
+        else if(i == numRanges-1) //tack on to end
+        {
+          if(i==0)
+            llLoAsc[llLoAscHead] = numRanges;
+          else
+            llLoAsc[llLoAsc[llIdxPrev]] = numRanges;
+        }
+        
+        llIdxPrev = i==0 ? llLoAscHead : llLoAsc[llIdxPrev];
+      }
+    }
+
+    numRanges++;
+  }
+  
+  //Consolidate ranges
+  size_t consIdx = llLoAscHead;
+  size_t idx = llLoAsc[consIdx];
+  int origNumRanges = numRanges;
+  for(int i=1; i<origNumRanges; i++)
+  {
+    uint64_t thisLo = lo[idx], thisHi = hi[idx];
+
+    if(thisLo <= hi[consIdx] + 1) //within the current cons, so extend it
+    {
+      hi[consIdx] = MAX(thisHi, hi[consIdx]);
+      llLoAsc[consIdx] = llLoAsc[idx]; //now it's included, skip it
+      numRanges--;
+    }
+    else //start new cons
+    {
+      consIdx = idx;
+    }
+
+    idx = llLoAsc[idx];
+  }
+
+  //Sum ranges
+  uint64_t sum=0;
+  idx = llLoAscHead;
+  for(int i=0; i<numRanges; i++)
+  {
+    sum += hi[idx] - lo[idx] + 1;
+    idx = llLoAsc[idx];
+  }
+
+  printf("%llu", sum);
 }
